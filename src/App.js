f5b58d62879cb179b38b90daecf589f26662e404
desmoteuss <desmoteuss@gmail.com>
2020-10-07 15:47:04 +0200

    adding snakes food ramdomizer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import Snake from './snake'
+import Food from './food'
+
+
+
+const getRandomCoords = () => { 
+  let min = 1;
+  let max = 98;
+  let randX = Math.floor((Math.random()*(max-min+1)+min)/2)*2;
+  let randY = Math.floor((Math.random()*(max-min+1)+min)/2)*2;
+  return [randX,randY]
+}
 
 class App extends Component {
 state = { 
-  snakeDots: [ 
+  food: getRandomCoords(),
+    snakeDots: [ 
     [0,0],[2,0]
   ]
 
@@ -15,8 +27,10 @@ state = {
   return (
    <div className="game-area">
      <Snake snakeDots= { this.state.snakeDots}/>
+     <Food  dot={this.state.food}/>
 
    </div>
+   
    );
   }
 }
